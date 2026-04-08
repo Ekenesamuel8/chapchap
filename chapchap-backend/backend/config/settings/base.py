51a9f0b402f2ca1be5f6f2bd9386e11da2b276b9
ekenesamuel <chikwenduekene8@gmail.com>
2026-04-08 19:16:54 +0100

    backend

--- chapchap-backend/backend/config/settings/base.py
+++ chapchap-backend/backend/config/settings/base.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get(
     "django-insecure-change-me",
 )
 DEBUG = _get_bool("DEBUG", default=False)
-ALLOWED_HOSTS = _get_list("ALLOWED_HOSTS", default=["127.0.0.1", "localhost"])
+ALLOWED_HOSTS = _get_list("ALLOWED_HOSTS", default=["127.0.0.1", "localhost", "https://chapchap-rho.vercel.app"])
 
 DJANGO_APPS = [
     "django.contrib.admin",
@@ -76,6 +76,7 @@ ROOT_URLCONF = "config.urls"
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://chapchap-rho.vercel.app",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
