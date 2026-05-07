3ca703bc3eae65d31915e383d2fa411c570cce98
ekenesamuel <chikwenduekene8@gmail.com>
2026-05-07 19:42:18 +0100

    frontend done

--- chapchap-frontend/next.config.ts
+++ chapchap-frontend/next.config.ts
@@ -4,7 +4,14 @@ const nextConfig: NextConfig = {
   async headers() {
     return [
       {
-        source: "/:path*",
+        source: "/dashboard/:path*",
+        headers: [
+          { key: "Cross-Origin-Opener-Policy", value: "same-origin" },
+          { key: "Cross-Origin-Embedder-Policy", value: "require-corp" },
+        ],
+      },
+      {
+        source: "/wallet/:path*",
         headers: [
           { key: "Cross-Origin-Opener-Policy", value: "same-origin" },
           { key: "Cross-Origin-Embedder-Policy", value: "require-corp" },
